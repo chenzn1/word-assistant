34b7f2d57610e9fed75027bc1f3fb6600572728d
Zn <312073339@qq.com>
2022-08-20 15:23:50 +0800

    feat: init client

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,15 +1,15 @@
-import Koa from 'koa'
-import Router from 'koa-router'
+import Koa from "koa";
+import Router from "koa-router";
 
 export function createApp() {
-  const app = new Koa()
-  const router = new Router()
+  const app = new Koa();
+  const router = new Router();
 
-  router.get('/', (ctx, next) => {
+  router.get("/", (ctx, next) => {
     // ctx.router available
-    ctx.body = { words: ['a', 'b'] }
-  })
+    ctx.body = { words: ["a", "b"] };
+  });
 
-  app.use(router.routes()).use(router.allowedMethods())
-  return app
+  app.use(router.routes()).use(router.allowedMethods());
+  return app;
 }
