6295602791df4d6a643850fee89aec3ac6788c4c
Zn <312073339@qq.com>
2022-08-20 15:42:52 +0800

    chore: pre commit

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,15 +1,14 @@
-import Koa from "koa";
-import Router from "koa-router";
+import Koa from 'koa'
+import Router from 'koa-router'
 
-export function createApp() {
-  const app = new Koa();
-  const router = new Router();
+export function createAp() {
+  const app = new Koa()
+  const router = new Router()
+  router.get('/', (ctx, next) => {
+    // ctx.router availab'e'''
+    ctx.body = { words: ['a', 'b'] }
+  })
 
-  router.get("/", (ctx, next) => {
-    // ctx.router available
-    ctx.body = { words: ["a", "b"] };
-  });
-
-  app.use(router.routes()).use(router.allowedMethods());
-  return app;
+  app.use(router.routes()).use(router.allowedMethods())
+  return app
 }
